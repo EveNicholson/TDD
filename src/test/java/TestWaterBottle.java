30ac90350b81265255dc094b18e03882f0f1b999
Ewelina Nicholson <e.guzikk@gmail.com>
2023-09-19 10:31:31 +0100

    first commit

---
+++ src/test/java/TestWaterBottle.java
@@ -0,0 +1,39 @@
+import com.codebase.WaterBottle;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class TestWaterBottle {
+    private WaterBottle waterBottle;
+
+    @Before
+    public void setUp() {
+        waterBottle = new WaterBottle();
+    }
+
+    @Test
+    public void hasVolume() {
+
+        assertEquals(100, waterBottle.getVolume());
+    }
+
+    @Test
+    public void RemovesVolume10() {
+        waterBottle.drink();
+        assertEquals(90, waterBottle.getVolume());
+    }
+
+    @Test
+    public void SetVolumeTo0() {
+        waterBottle.empty();
+        assertEquals(0, waterBottle.getVolume());
+    }
+
+    @Test
+    public void fillVolumeTo100() {
+        waterBottle.empty();
+        waterBottle.fill();
+        assertEquals(100, waterBottle.getVolume());
+    }
+}
